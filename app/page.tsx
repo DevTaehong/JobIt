f8f6324e569147018cca704d0168ea551f272367
DevTaehong <minth1123@icloud.com>
2023-07-11 15:58:09 -0300

    chore: Re-setup ESLint and Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
@@ -9,7 +7,12 @@ export default function Home() {
       <h1>Jobit</h1>
       {/* Just test for components */}
       <Input type="email" placeholder="Email" />
-      <Button type="submit" className="flex pt-[12px] pb-[12px] pl-[14px] pr-[14px] bg-[#0BAB7C]">Subscribe</Button>
+      <Button
+        type="submit"
+        className="flex bg-sky-700 px-4 py-2 text-white sm:px-8"
+      >
+        Subscribe
+      </Button>
     </>
   );
 }
