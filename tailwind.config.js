a69cc650ec44db1316f4c5bc43bc79a66dbb3025
fcvirtual <austin@fcvt.dev>
2023-07-11 12:49:03 -0400

    updates to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // @type {import('tailwindcss').Config}
-module.export = {
-  content: [
+module.exports = {
+    content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
