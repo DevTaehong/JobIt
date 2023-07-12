e9512891de5890a7e00c4b72fe25120fbc0c31fa
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-07-12 07:41:04 -0500

    toast component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function Home() {
+  const { toast } = useToast();
+
   return (
     <>
       <h1>Jobit</h1>
@@ -9,9 +14,27 @@ export default function Home() {
       <Input type="email" placeholder="Email" />
       <Button
         type="submit"
-        className="flex bg-sky-700 px-4 py-2 text-white sm:px-8"
+        className=" flex bg-green-700 px-4 py-2 text-white sm:px-8"
+        onClick={() => {
+          toast({
+            variant: "destructive",
+            title: "Uh oh! Something went wrong.",
+            description: "Thank You for subscribing to our Newsletter",
+          });
+        }}
       >
         Subscribe
+      </Button>
+      <Button
+        className=" flex bg-teal-500 px-4 py-2 text-white sm:px-8"
+        variant="outline"
+        onClick={() => {
+          toast({
+            description: "Your message has been sent.",
+          });
+        }}
+      >
+        Show Toast
       </Button>
     </>
   );
