d01e69fdc7b23f7296f36a82f45af95e816b0c15
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-07-13 08:58:16 -0500

    Toast component

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,12 @@
 "use client";
 
-import InputValidationExample from "@/components/InputValidationExample";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useToast } from "@/components/ui/use-toast";
+// import InputValidationExample from "@/components/InputValidationExample";
+// import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
-  const { toast } = useToast();
-
   return (
     <div className="m-auto flex w-60 flex-col justify-items-center gap-4">
-      {/* Just test for components */}
-      <div className="flex items-center space-x-4">
-        <Skeleton className="h-12 w-12 rounded-full" />
-        <div className="space-y-2">
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
-        </div>
-      </div>
-      <InputValidationExample />
+      <h1>Jobit</h1>
     </div>
   );
 }
