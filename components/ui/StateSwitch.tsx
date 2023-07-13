0f7c930c2cfb8b9a650a704ac7b3f4695073e2dc
fcvirtual <austin@fcvt.dev>
2023-07-13 09:02:03 -0400

    swap function

--- components/ui/StateSwitch.tsx
+++ components/ui/StateSwitch.tsx
@@ -2,31 +2,29 @@
 "use client"
 
 import { Switch } from "@/components/ui/switch"
-import { useSelector, useDispatch } from "react-redux";
-import { setLight, setDark } from "@/app/context/feature/theme/themeSlice";
-import { RootState } from "@/app/context/feature/store";
-import { useEffect } from "react";
+
 
 export default function StateSwitch() {
-    const dispatch = useDispatch()
-    const theme = useSelector((state: RootState) => state.theme.value)
     const swap = (check: boolean) => {
         if (check) {
-            dispatch(setDark())
+            document.documentElement.classList.add('dark')
         } else {
-            dispatch(setLight())
+            document.documentElement.classList.remove('dark')
         }
+        // Whenever the user explicitly chooses light mode
+        localStorage.theme = 'light'
+
+        // Whenever the user explicitly chooses dark mode
+        localStorage.theme = 'dark'
+
+        // Whenever the user explicitly chooses to respect the OS preference
+        localStorage.removeItem('theme')
     }
 
-    useEffect(() => {
-        document.body.classList.remove('light')
-        document.body.classList.remove('dark')
-        document.body.classList.add(theme)
-    }, [theme])
 
 
 
-    
+
     return (
         <>
             <div className="flex h-screen items-center justify-center">
