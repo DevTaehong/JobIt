ba78e450854f072308fac6f5ff4bd3b31be34a3a
Markus <williams.markus18@yahoo.com>
2023-07-14 17:23:05 -0500

    moved all assets to public folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,16 +10,29 @@ module.exports = {
   ],
   theme: {
       colors: {
-        Black: "#000700",
-        Black1: "#171725",
-        Natural:"#92929D",
-        Natural2:"#f1f1f5",
-        Natural3:"#696974",
-        Natural4:"#fafafb",
-        Natural5:"#f5f5f8",
-        Natural6:"#e2e2ea",
-        PrimaryColor:"#0bab7c",
-        White:"#FFFFF",
+      Primary:"0BAB7C",
+      Secondary1:"#C7F4C2",
+      Secondary2:"#D7D0FF",
+      Secondary3:"FDDD8C",
+      Secondary4:"FFBBD7",
+      Natural1:"#FAFAF4",
+      Natural2:"#F1F1F5",
+      Natural3:"#FaFaFb",
+      Natural4:"#F5F5F8",
+      Natural5:"#E2E2EA",
+      Natural6:"#92929D",
+      Natural7:"#696974",
+      Natural8:"#44444F",
+      DarkBG1:"#13131A",
+      DarkBG2:"#1C1C24",
+      DarkBG3:"#21212B",
+      White:"#FFFFFF",
+      Black:"#171725"
+
+
+
+
+
 
       },
       fontFamily: {
