fc7b19d4d83018e87d34507728b2e7d92c7bedc6
DevTaehong <minth1123@icloud.com>
2023-07-17 16:22:20 -0300

    Commit before merge featrue/navBar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,15 +3,32 @@ import Image from "next/image";
 import Link from "next/link";
 import StateSwitch from "./StateSwitch";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <header>
-      <nav className="flex h-[4.375rem] w-full shrink-0 items-center justify-between gap-[0.625rem] border-b border-solid border-Natural5 bg-White px-20 py-0 font-manrope dark:border-DarkBG3 dark:bg-DarkBG1">
+      <nav className="flex h-[4.375rem] w-full shrink-0 items-center justify-between gap-[0.625rem] border-b border-solid border-Natural5 bg-White px-[1.5rem] py-[1.125rem] font-manrope dark:border-DarkBG3 dark:bg-DarkBG1 md:px-20 md:py-0">
+        {/* Mobile Menu */}
+        <button
+          className="flex md:hidden"
+          onClick={() => {
+            setOpen((prev) => !prev);
+          }}
+        >
+          <Image
+            width={24}
+            height={24}
+            alt="Menu"
+            src="/iconography/companyLogo-outline-property.svg"
+          />
+        </button>
+
         <Link
           href="/"
-          className="inline-flex items-center gap-[0.375rem] pb-0 pl-[0.125rem] pr-[0.0625rem] pt-[0.125rem]"
+          className="hidden items-center gap-[0.375rem] pb-0 pl-[0.125rem] pr-[0.0625rem] pt-[0.125rem] md:inline-flex"
         >
           <Image src="/images/Logo.svg" alt="logo" width={20} height={20} />
           <Image
@@ -22,7 +39,7 @@ const Navbar = () => {
           />
         </Link>
 
-        <div className="flex w-auto">
+        <div className="hidden w-auto md:flex">
           <ul className="flex gap-[1.875rem]">
             <li>
               <NavBarLink href="/">Overview</NavBarLink>
@@ -70,12 +87,12 @@ function NavBarLink({
   const selectedClass =
     pathName === href
       ? "border-b border-solid border-Primary text-Primary font-bold"
-      : "";
+      : "font-medium";
 
   return (
     <Link
       href={href}
-      className={`${selectedClass} px-0 py-[1.4375rem] font-manrope text-base font-medium not-italic text-Natural6 hover:text-Primary`}
+      className={`${selectedClass} px-0 py-[1.4375rem] font-manrope text-base not-italic text-Natural6 hover:text-Primary`}
     >
       {children}
     </Link>
