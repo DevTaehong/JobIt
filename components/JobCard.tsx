ebcc3322c05d8511789936d1ccd21063b381f866
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-07-17 13:47:17 -0500

    JobCard fix

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,18 +1,26 @@
 import React from "react";
-
+import icMore from "@/public/static/iconography/ic_More.svg";
+import briefcase from "@/public/static/iconography/outline-briefcase.svg";
+import companyLogo from "@/public/static/iconography/companyLogo-outline-property.svg";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 type Props = {};
 
 const JobCard = (props: Props) => {
   return (
     <div className="flex w-full flex-col gap-7 bg-white p-5">
-      <div className="flex">
-        <img src="app/favicon.ico" alt="logo" />
-        <div className="flex flex-col">
-          <h1>Passionate Programmer</h1>
-          <p>PHP</p>
+      <div className="flex justify-between">
+        <div className="flex">
+          <Image src={companyLogo} alt="logo" />
+          <div className="flex flex-col pl-5">
+            <h1>Passionate Programmer</h1>
+            <div className="inline-flex">
+              <p className="rounded bg-Natural3 px-2 py-1 text-Natural6">PHP</p>
+            </div>
+          </div>
         </div>
+        <Image className="items-start" src={icMore} alt="More Icon" />
       </div>
       <div>
         <p>
@@ -22,18 +30,24 @@ const JobCard = (props: Props) => {
           skillset to join our team and grow with us.
         </p>
       </div>
-      <div className="flex">
-        <div className="flex">
-          <img src="app/favicon.ico" alt="logo" />
+      <div className="flex justify-between">
+        <div className="flex justify-center rounded bg-Natural3 px-2 py-1 text-Natural6">
+          <Image className="mr-2" src={briefcase} alt="logo" />
           <p>Full Time</p>
         </div>
-        <div className="flex">
-          <img src="app/favicon.ico" alt="logo" />
+        <div className="flex justify-center rounded bg-Natural3 px-2 py-1 text-Natural6">
+          <Image className="mr-2" src={briefcase} alt="logo" />
+          <p>Full Time</p>
+        </div>
+        <div className="flex justify-center rounded bg-Natural3 px-2 py-1 text-Natural6">
+          <Image className="mr-2" src={briefcase} alt="logo" />
           <p>Full Time</p>
         </div>
       </div>
       <div className="flex items-center justify-between">
-        <h3>$15k-20k/month</h3>
+        <h3 className="text-black">
+          $15k-20k<span className="text-Natural7">/month</span>
+        </h3>
         <Button className="h-12 w-24 items-center justify-center rounded-lg bg-Primary p-3">
           Visit Now
         </Button>
