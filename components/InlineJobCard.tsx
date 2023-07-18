19bcf0df4cd8d0326901e18f012573066c61e833
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-07-18 11:46:23 -0500

    InlineJobCard

---
+++ components/InlineJobCard.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Image from "next/image";
+
+type Props = {
+  employerName: string;
+  jobTitle: string;
+  salary: number | null;
+  salaryPeriod: string | null;
+  companyLogo: string;
+  jobState: string;
+  jobCity: string;
+  employmentType: string;
+};
+
+const InlineJobCard = (props: Props) => {
+  return (
+    <div className="hidden w-full flex-col gap-5 bg-Natural3 p-5 font-manrope sm:block">
+      <div className="flex justify-between">
+        <div className="flex rounded">
+          <div className="rounded p-2">
+            <img
+              width={36}
+              className="h-9 w-9"
+              src={props.companyLogo}
+              alt="logo"
+            />
+          </div>
+          <div className="flex flex-col pl-5">
+            <h1 className=" text-lg font-semibold text-gray-900">
+              {props.jobTitle}
+            </h1>
+            <p className="text-center text-sm font-medium leading-tight text-Natural7">
+              {`${props.employerName} ･ ${props.jobCity},${props.jobState}`}
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col items-start">
+          {props.salary && (
+            <h3 className="text-black">
+              {`$${props.salary}/`}
+              <span className="font-sans text-Natural7">
+                {props.salaryPeriod}
+              </span>
+            </h3>
+          )}
+          <p className="text-center text-sm font-medium leading-tight text-Natural7">
+            {props.employmentType}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InlineJobCard;
