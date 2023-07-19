e64fd9fd88cb774de309049b7e1c0a141cb7729e
DevTaehong <minth1123@icloud.com>
2023-07-19 12:46:16 -0300

    Correct spelling

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import moment from "moment";
 
 export default function Home() {
-  // const currentDate = new Date().toDateString();
   const currentDate = moment().format("dddd,  D MMM YYYY");
 
   return (
@@ -55,11 +54,11 @@ export default function Home() {
                 </div>
               </div>
             </section>
-            {/* Recomended For You */}
+            {/* Recommended For You */}
             <section className="order-last mt-[2.19rem] w-full sm:order-none sm:w-1/3">
               <span className="ml-0 flex justify-between sm:ml-10">
                 <h3 className=" text-[1.375rem] font-bold leading-8 dark:text-White">
-                  Recomended For You
+                  Recommended For You
                 </h3>
                 <button className="flex items-center gap-2 whitespace-nowrap border px-[.62rem] py-[.44rem] font-sans text-xs	 font-medium leading-6 text-Natural7 dark:border-DarkBG3">
                   See All
@@ -81,7 +80,7 @@ export default function Home() {
                 <div>Inline Job Card</div>
               </div>
             </section>
-            {/* Featured Compaines */}
+            {/* Featured Companies */}
             <section className=" mt-10 w-full lg:w-1/3">
               <span>
                 <h3 className=" text-2xl font-bold leading-10 dark:text-White">
