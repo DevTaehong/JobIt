9b51dc06f9fd6272f5c7d7744605ca847b1f2ba7
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-07-20 09:02:07 -0500

    bg border on company logo

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -30,71 +30,73 @@ const JobCard = (props: Props) => {
   const daysLeft = calculateDaysLeft(props.expirationDate);
 
   return (
-    <div className="flex w-full flex-col gap-7 bg-white p-5 font-manrope">
-      <div className="flex justify-between">
-        <div className="flex rounded">
-          <div className="rounded p-2">
-            <img
-              className="h-12 min-w-[48px]"
-              src={props.companyLogo}
-              alt="logo"
-            />
-          </div>
-          <div className="flex flex-col pl-5">
-            <h1 className=" text-lg font-semibold text-gray-900">
-              {props.jobTitle}
-            </h1>
+    <div className="">
+      <div className="flex flex-col gap-7 bg-white p-5 font-manrope">
+        <div className="flex justify-between">
+          <div className="flex rounded">
+            <div className="rounded-[10px] border border-neutral-50 bg-Natural3 p-2">
+              <img
+                className="h-12 min-w-[48px] "
+                src={props.companyLogo}
+                alt="logo"
+              />
+            </div>
+            <div className="flex flex-col justify-between pl-5">
+              <h1 className=" text-lg font-semibold text-gray-900">
+                {props.jobTitle}
+              </h1>
 
-            {/* Conditionally render skills */}
-            <div className="flex gap-1">
-              {props.jobSkills &&
-                props.jobSkills.map((skill, index) => (
-                  <div className="flex" key={index}>
-                    <p className="justify-start rounded bg-Natural3 px-2 py-1 text-sm text-Natural6">
-                      {skill}
-                    </p>
-                  </div>
-                ))}
+              {/* Conditionally render skills */}
+              <div className="flex gap-1">
+                {props.jobSkills &&
+                  props.jobSkills.map((skill, index) => (
+                    <div className="flex" key={index}>
+                      <p className="justify-start rounded bg-Natural3 px-2 py-1 text-sm text-Natural6">
+                        {skill}
+                      </p>
+                    </div>
+                  ))}
+              </div>
             </div>
           </div>
+          <div>
+            <Image className="" src={icMore} alt="More Icon" />
+          </div>
         </div>
+
         <div>
-          <Image className="" src={icMore} alt="More Icon" />
+          <p className="line-clamp-6 text-Natural7">{props.jobDescription}</p>
         </div>
-      </div>
 
-      <div>
-        <p className="line-clamp-6 text-Natural7">{props.jobDescription}</p>
-      </div>
+        <div className="flex justify-between">
+          <div className="flex items-center justify-center rounded bg-Natural3 px-2 py-1 text-sm text-Natural6">
+            <Image className="mr-2" src={briefcase} alt="logo" />
+            <p>{props.employmentType}</p>
+          </div>
+          <div className="flex items-center justify-center rounded bg-Natural3 px-2 py-1 text-sm text-Natural6">
+            <Image className="mr-2" src={briefcase} alt="logo" />
+            <p>Full Time</p>
+          </div>
+          <div className="flex items-center justify-center rounded bg-Natural3 px-2 py-1 text-sm text-Natural6">
+            <Image className="mr-2" src={briefcase} alt="logo" />
+            <p>{`${daysLeft} days left`}</p>
+          </div>
+        </div>
 
-      <div className="flex justify-between">
-        <div className="flex items-center justify-center rounded bg-Natural3 px-2 py-1 text-sm text-Natural6">
-          <Image className="mr-2" src={briefcase} alt="logo" />
-          <p>{props.employmentType}</p>
+        {/* Conditionally render salaries */}
+        <div className="flex items-center justify-between">
+          {props.salary && (
+            <h3 className="text-black">
+              {`$${props.salary}/`}
+              <span className="font-sans text-Natural7">
+                {props.salaryPeriod}
+              </span>
+            </h3>
+          )}
+          <Button className="h-12 w-24 items-center justify-center rounded-lg bg-Primary p-3">
+            Visit Now
+          </Button>
         </div>
-        <div className="flex items-center justify-center rounded bg-Natural3 px-2 py-1 text-sm text-Natural6">
-          <Image className="mr-2" src={briefcase} alt="logo" />
-          <p>Full Time</p>
-        </div>
-        <div className="flex items-center justify-center rounded bg-Natural3 px-2 py-1 text-sm text-Natural6">
-          <Image className="mr-2" src={briefcase} alt="logo" />
-          <p>{`${daysLeft} days left`}</p>
-        </div>
-      </div>
-
-      {/* Conditionally render salaries */}
-      <div className="flex items-center justify-between">
-        {props.salary && (
-          <h3 className="text-black">
-            {`$${props.salary}/`}
-            <span className="font-sans text-Natural7">
-              {props.salaryPeriod}
-            </span>
-          </h3>
-        )}
-        <Button className="h-12 w-24 items-center justify-center rounded-lg bg-Primary p-3">
-          Visit Now
-        </Button>
       </div>
     </div>
   );
