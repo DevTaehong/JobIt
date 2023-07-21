f0aa519b0ba0d7690ffdb58cda19f63c57184699
fcvirtual <austin@fcvt.dev>
2023-07-21 16:41:00 -0400

    header size

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <main className="mx-6 w-auto font-manrope sm:mx-20">
           {/* Heading */}
           <section className=" ml-[-.25] mt-[3.13] sm:mt-[1.5rem]">
-            <h1 className="text-[1.375rem] font-bold not-italic leading-8 dark:text-Natural4 sm:text-2xl sm:leading-10">
+            <h1 className="text-[1.375rem] font-bold not-italic leading-8 dark:text-Natural4 sm:text-[2rem] sm:leading-10">
               Welcome to the Job Search Platform for Developers
             </h1>
             {/* Date */}
