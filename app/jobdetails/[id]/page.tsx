2e90589b574fb4529e2651c9019b7e2bb2a5a5d4
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-07-22 14:26:02 -0500

    Similar jobs Section

--- app/jobdetails/[id]/page.tsx
+++ app/jobdetails/[id]/page.tsx
@@ -19,16 +19,34 @@ const JobDetails = ({ params }: { params: { id: string } }) => {
           </h2>
         </section>
         {/* Job Details */}
-        <section className="mt-8 w-full xl:w-2/3">
-          <span className="flex items-center sm:mb-[23px] ">
-            <button className="hidden items-center gap-2 rounded-[0.625rem] bg-Natural4 px-[.62rem] py-[.44rem] text-xs font-medium leading-[18px] text-Natural7 dark:border-DarkBG3 sm:flex">
-              Back
-            </button>
-          </span>
-          <div>
-            <JobDetailCard qualifications={["2, ", "1"]} />
-          </div>
-        </section>
+        <div className="flex flex-row flex-wrap sm:flex-nowrap sm:gap-10">
+          <section className="mt-8 w-full xl:w-2/3">
+            <span className="flex items-center sm:mb-[23px] ">
+              <button className="hidden items-center gap-2 rounded-[0.625rem] bg-Natural4 px-[.62rem] py-[.44rem] text-xs font-medium leading-[18px] text-Natural7 dark:border-DarkBG3 sm:flex">
+                Back
+              </button>
+            </span>
+            <div className="rounded-[10px] bg-white">
+              <JobDetailCard qualifications={["2, ", "1"]} />
+            </div>
+          </section>
+
+          {/* Similar Jobs */}
+          <section className="order-last mt-[2.19rem] w-full xl:order-none xl:w-1/3">
+            <span className="flex justify-between">
+              <h3 className="text-[18px] font-bold leading-8 dark:text-White">
+                Similar Jobs
+              </h3>
+            </span>
+            {/* Similar Job Cards */}
+            <div className="mt-[2.06rem] flex-row gap-3">
+              <div>Similar Job 1</div>
+              <div>Similar Job 2</div>
+              <div>Similar Job 3</div>
+              <div>Similar Job 4</div>
+            </div>
+          </section>
+        </div>
       </main>
     </>
   );
