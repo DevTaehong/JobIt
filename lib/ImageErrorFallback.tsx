7cf98a6332126f5fbd3b9ab2dcd158b668b8b8d4
DevTaehong <minth1123@icloud.com>
2023-07-26 23:20:31 -0300

    Add a fallback function for images

--- lib/ImageErrorFallback.tsx
+++ lib/ImageErrorFallback.tsx
@@ -5,26 +5,22 @@ export default function ImageErrorFallback({ src }: { src: string }) {
   const [error, setError] = useState(false);
   if (!src || error)
     return (
-      <div className="h-9 w-9 rounded py-1.5 pr-2">
-        <img
-          className="min-h-[30px] min-w-[30px] object-contain sm:max-h-[36px] sm:max-w-[36px]"
-          alt="Company placeholder logo"
-          src="/images/companyPlaceholderLogo.png"
-        />
-      </div>
+      <img
+        className="min-h-[30px] min-w-[30px] sm:max-h-[36px] sm:max-w-[36px]"
+        alt="Company placeholder logo"
+        src="/images/companyPlaceholderLogo.png"
+      />
     );
   if (!error) {
     return (
-      <div className="h-9 w-9 rounded py-1.5 pr-2">
-        <img
-          className="min-h-[30px] min-w-[30px] object-contain sm:max-h-[36px] sm:max-w-[36px]"
-          alt="Company Logo"
-          src={src}
-          onError={(e) => {
-            setError(true);
-          }}
-        />
-      </div>
+      <img
+        className="min-h-[30px] min-w-[30px] sm:max-h-[36px] sm:max-w-[36px]"
+        alt="Company Logo"
+        src={src}
+        onError={(e) => {
+          setError(true);
+        }}
+      />
     );
   }
 }
