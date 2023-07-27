ed26ee28b00becd8621efdc9aae7c08cd98d197e
DevTaehong <minth1123@icloud.com>
2023-07-27 20:37:49 -0300

    Create a formatSalaryRange function

--- lib/formatSalaryRage.tsx
+++ lib/formatSalaryRage.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
 
 export const formatSalaryRange = (
   minSalary: number,
   maxSalary: number,
   salaryPeriod: string,
 ): JSX.Element => {
-  const formattedMin = (minSalary / 1000).toFixed(0) + "k";
-  const formattedMax = (maxSalary / 1000).toFixed(0) + "k";
+  const formattedMin = minSalary / 1000 + "k";
+  const formattedMax = maxSalary / 1000 + "k";
 
   return (
-    <span>{`$${formattedMin}-${formattedMax}/${salaryPeriod.toLowerCase()}`}</span>
+    <>
+      <span className="text-base font-semibold text-Natural8">{`$${formattedMin}-${formattedMax}`}</span>
+      <span className="text-sm font-medium not-italic text-Natural7">{`/${salaryPeriod.toLowerCase()}`}</span>
+    </>
   );
 };
