e08688775da7531bb590295bb87c1b7f0e48f85b
Markus <williams.markus18@yahoo.com>
2023-07-30 16:16:27 -0500

    fixes

--- app/companydetails/page.tsx
+++ app/companydetails/page.tsx
@@ -1,5 +1,13 @@
+import SimilarCompanies from "@/components/SimilarCompanies";
+
+
 const CompanyDetails = () => {
-  return <div>CompanyDetails</div>;
+  return <div><SimilarCompanies
+JobTitle="Apple"
+JobTitleSec="Apple Inc"
+icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKlgydP7sElaJC9qPrtNHwBhyTMHYgii1RPWsy&s=0"
+/></div>;
+
 };
 
 export default CompanyDetails;
