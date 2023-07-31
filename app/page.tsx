c69b0a8733204d38719911f3869b1fd3cb7d84ec
fcvirtual <austin@fcvt.dev>
2023-07-31 13:24:05 -0400

    screen with - margin tops - font weight

--- app/page.tsx
+++ app/page.tsx
@@ -15,23 +15,23 @@ export default async function Home() {
   const [RecommendedJobs] = await Promise.all([RecommendedJobData]);
 
   return (
-    <main className="mx-6 w-auto md:mx-20">
+    <main className="mx-6	max-w-screen-2xl md:mx-20">
       {/* Heading */}
-      <section className=" ml-[-.25] mt-[3.13] sm:mt-[3rem]">
+      <section className=" ml-[-.25] mt-[3.13] sm:mt-[3.12rem]">
         <h1 className="text-[1.375rem] font-bold not-italic leading-8 dark:text-Natural4 sm:text-[2rem] sm:leading-10">
           Welcome to the Job Search Platform for Developers
         </h1>
         {/* Date */}
-        <h2 className="hidden text-Natural6 sm:flex sm:text-xl sm:font-medium sm:leading-8 xl:pt-[.88rem]">
+        <h2 className="hidden text-Natural6 sm:flex sm:text-xl sm:font-medium sm:leading-8 xl:mt-[.88rem]">
           {currentDate}
         </h2>
       </section>
       <div className="flex flex-row flex-wrap">
         {/* Latest Job Posts */}
-        <section className="mt-8 w-full xl:w-2/3">
+        <section className="mt-[3.56rem] w-full xl:mt-[2.19rem] xl:w-2/3">
           <div className="flex-row ">
-            <span className="flex items-center justify-between sm:mb-[2.06rem] ">
-              <h3 className="text-[1.375rem] font-bold leading-8 dark:text-White">
+            <span className="mb-[2.06rem] flex items-center justify-between sm:mb-[2.06rem]">
+              <h3 className="text-[1.375rem] font-semibold leading-8 dark:text-White md:font-bold">
                 Latest Job Posts
               </h3>
               <button className="flex items-center justify-center gap-2 rounded-[0.625rem] border px-[.62rem] py-[.44rem] text-xs font-medium leading-6 text-Natural7 marker:w-[5.9375rem] dark:border-DarkBG3 md:w-[4.4375rem] xl:items-center">
@@ -74,7 +74,7 @@ export default async function Home() {
         {/* Recommended For You */}
         <section className="order-last mt-[2.19rem] w-[25rem] xl:order-none xl:w-1/3 xl:pl-10">
           <span className="flex w-[20.4375rem] justify-between md:w-[25rem] 2xl:w-[33.5rem]">
-            <h3 className="w-[20.4375rem] text-[1.375rem] font-semibold leading-8 dark:text-White md:w-[25rem] md:font-bold">
+            <h3 className="w-[20.4375rem] text-[1.375rem] font-semibold	leading-8 dark:text-White md:w-[25rem] md:font-bold">
               Recommended For You
             </h3>
             <button className="flex w-[5.9375rem] items-center justify-center rounded-[0.625rem] border px-[0.625rem] py-[0.4375rem] text-xs font-medium leading-6 text-Natural7 dark:border-DarkBG3 md:w-[4.4375rem] xl:items-center">
@@ -110,7 +110,7 @@ export default async function Home() {
         {/* Featured Companies */}
         <section className="mt-10 w-full">
           <span>
-            <h3 className="text-2xl font-bold leading-10 dark:text-White">
+            <h3 className="text-2xl font-semibold leading-10 dark:text-White md:font-bold">
               Featured Companies
             </h3>
           </span>
