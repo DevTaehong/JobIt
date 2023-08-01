58873ef15bc9aed4d3800e056a7947057ee2bb8f
fcvirtual <austin@fcvt.dev>
2023-08-01 18:24:49 -0400

    salaries inputs

--- app/estimatedsalaries/page.tsx
+++ app/estimatedsalaries/page.tsx
@@ -1,6 +1,7 @@
 import moment from "moment";
 import Charts from "@/components/Charts";
 import { data } from "autoprefixer";
+import SalariesInputs from "@/components/SalariesInputs";
 
 const EstimatedSalaries = () => {
   const currentDate = moment().format("dddd,  D MMM YYYY");
@@ -43,18 +44,20 @@ const EstimatedSalaries = () => {
 
   return (
     <>
-      <div className="mx:mb-[5.75rem] mx-[1.5rem] mb-[4rem] mt-[2.5rem] md:mx-[5rem] md:mt-[3.5rem]">
+      <div className="mx:mb-[5.75rem] mx-[1.5rem] mb-[4rem] mt-[2.5rem] lg:mx-[5rem] lg:mt-[3.5rem]">
         <section>
-          <h1 className="ml-[.13rem] text-[1.375rem] font-bold not-italic leading-8 dark:text-Natural4 md:text-[2rem] md:leading-10">
+          <h1 className="ml-[.13rem] text-[1.375rem] font-bold not-italic leading-8 dark:text-Natural4 lg:text-[2rem] lg:leading-10">
             Estimated Salaries
           </h1>
-          <h2 className="mt-[.25rem] text-base font-medium leading-6 text-Natural6 md:mt-[.87rem] md:text-xl md:font-medium md:leading-8">
+          <h2 className="mt-[.25rem] text-base font-medium leading-6 text-Natural6 lg:mt-[.87rem] lg:text-xl lg:font-medium lg:leading-8">
             {currentDate}
           </h2>
         </section>
-        <div className="mt-[2.5rem] flex-row gap-[2.5rem] md:flex md:w-full">
-          <section className="md:w-1/2">{/* <SalariesInputs /> */}</section>
-          <section className="mt-[2.5rem] md:mt-[-6.5rem] md:h-[26.5rem] md:w-1/2">
+        <div className="mt-[2.5rem] flex-row gap-[2.5rem] lg:flex lg:w-full">
+          <section className="lg:h-[20.75rem] lg:w-1/2">
+            <SalariesInputs />
+          </section>
+          <section className="mt-[2.5rem] lg:mt-[-6.5rem] lg:h-[26.5rem] lg:w-1/2">
             <Charts data={data} />
           </section>
         </div>
