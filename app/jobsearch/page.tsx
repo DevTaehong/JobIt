d150a964e1b0297e55aebfd16b34ed60c3d5aa13
fcvirtual <austin@fcvt.dev>
2023-08-02 12:38:01 -0400

    layout and style

--- app/jobsearch/page.tsx
+++ app/jobsearch/page.tsx
@@ -5,7 +5,7 @@ const JobSearch = () => {
 
   return (
     <>
-      <div className="mx:mb-[5.75rem] mx-[1.5rem] mb-[4rem] mt-[2.5rem] lg:mx-[5rem] lg:mt-[3.5rem]">
+      <div className="mx-[1.5rem] mb-[4.06rem] mt-[2.5rem] flex flex-col lg:mx-[5rem] lg:mb-[3.62rem] lg:mt-[3.12rem]">
         {/* Heading */}
         <section>
           <h1 className="ml-[.13rem] text-[1.375rem] font-bold not-italic leading-8 dark:text-Natural4 lg:text-[2rem] lg:leading-10">
@@ -15,17 +15,19 @@ const JobSearch = () => {
             {currentDate}
           </h2>
         </section>
+        {/* Main */}
         {/* Search Bar */}
-        <section></section>
-        {/* Type of Employment & Job Cards */}
-        <section>
+        <section className="mt-[1.87rem] flex h-[20.43rem] w-auto flex-row bg-purple-500 lg:h-[5rem] lg:max-w-[80rem]"></section>
+        <section className="mt-[1.87rem] flex gap-[5rem] lg:mt-[3.69rem] lg:max-w-[80rem]">
           {/*  Type of Employment */}
-          <section></section>
+          <section className="hidden h-[64.9rem] w-[15.6rem] bg-green-500 lg:flex"></section>
           {/* Job Cards */}
-          <section></section>
+          <div className="flex w-[80rem] flex-col gap-[2.25rem]">
+            <section className="bg-yellow-500 lg:h-[50rem]"></section>
+            <section className="h-[2.25rem] bg-red-500"></section>
+          </div>
         </section>
         {/* Page Selector */}
-        <section></section>
       </div>
     </>
   );
