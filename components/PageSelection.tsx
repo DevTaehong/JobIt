7fd50b0f06c4b66c380c3826d57f330d8f18a6c3
fcvirtual <austin@fcvt.dev>
2023-08-04 13:43:56 -0400

    feat: page selector boarders and bgs

--- components/PageSelection.tsx
+++ components/PageSelection.tsx
@@ -24,7 +24,7 @@ const PageSelection = (props: Props) => {
               height={20}
             />
           </div>
-          <div className="flex text-[.875rem] font-semibold leading-6 text-Natural7 dark:text-Natural6">
+          <div className="flex text-[.875rem] font-semibold leading-6 text-Natural7  dark:text-Natural6">
             <p className="flex gap-[.2rem]">
               Page
               <p className="text-Natural8 dark:text-white">1</p>
@@ -49,9 +49,9 @@ const PageSelection = (props: Props) => {
           </div>
         </div>
         {/* Desktop */}
-        <div className="hidden w-full items-center justify-between xl:flex">
+        <div className="hidden w-full items-center justify-between border-t border-t-Natural2 pt-[1rem] dark:border-t-DarkBG3 xl:flex">
           {/* Previous */}
-          <div className="flex">
+          <div className="flex items-center justify-center gap-[.5rem] rounded-[.5rem] border border-Natural4 bg-white px-[.875rem] py-[.5rem] dark:border-DarkBG3 dark:bg-DarkBG2">
             <Image
               className="flex dark:hidden"
               src="/iconography/arrow-left.svg"
@@ -66,7 +66,9 @@ const PageSelection = (props: Props) => {
               width={20}
               height={20}
             />
-            <p>Previous</p>
+            <p className="text-[.875rem] font-bold leading-6 dark:text-Natural5">
+              Previous
+            </p>
           </div>
           {/* Page Numbers */}
           <div className="flex items-center gap-[.13rem] text-[.875rem] font-semibold leading-6 dark:text-Natural7">
@@ -78,7 +80,9 @@ const PageSelection = (props: Props) => {
           </div>
           {/* Next */}
           <div className="flex">
-            <p>Next</p>
+            <p className="text-[.875rem] font-bold leading-6 dark:text-Natural5">
+              Next
+            </p>
             <Image
               className="flex dark:hidden"
               src="/iconography/arrow-right.svg"
