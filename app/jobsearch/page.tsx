8f875d31db1cd5807c15a900b3954154892d46d1
DevTaehong <minth1123@icloud.com>
2023-08-04 01:38:32 -0300

    chore: Clean code up

--- app/jobsearch/page.tsx
+++ app/jobsearch/page.tsx
@@ -2,11 +2,9 @@ import SearchBar from "@/components/SearchBar";
 
 const JobSearch = () => {
   return (
-    <>
-      <div className="w-full rounded-[20px] bg-white px-1.5 py-4 shadow dark:bg-DarkBG2">
-        <SearchBar />
-      </div>
-    </>
+    <div className="w-full rounded-[20px] bg-white px-1.5 py-4 shadow dark:bg-DarkBG2">
+      <SearchBar />
+    </div>
   );
 };
 
